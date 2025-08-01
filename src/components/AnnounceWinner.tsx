0779251455d0336d3abe8210868701b482f31e0c
Sandha Saab <sandhasaab143@gmail.com>
2025-08-01 21:06:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/AnnounceWinner.tsx
+++ src/components/AnnounceWinner.tsx
@@ -15,7 +15,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/
 import { NumberRoller } from './NumberRoller';
 import withAdminAuth from './withAdminAuth';
 import { db } from '@/lib/firebase';
-import { doc, onSnapshot, collection, query, where, getDocs } from 'firebase/firestore';
+import { doc, onSnapshot, collection, query, where, getDocs, getDoc } from 'firebase/firestore';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './ui/accordion';
 
 const STAGE_CONFIG = {
